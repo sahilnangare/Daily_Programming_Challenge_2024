67d1c2adacd289dc2a10fd4a481d958dc7422dc6
sahilnangare <160593860+sahilnangare@users.noreply.github.com>
2024-09-09 10:44:28 +0530

    Add files via upload

---
+++ Sort_an_Array_of_0s_1s_and_2s.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void sortColors(int[] arr) {
+        int n = arr.length;
+        int mid=0, hi=n-1 , lo=0;
+        while(mid<=hi){
+            if (arr[mid]==0){
+                int temp =arr[mid];
+                arr[mid]=arr[lo];
+                arr[lo] = temp;
+                lo++; mid++;
+            }
+            else if (arr[mid]==1) mid++;
+            else {int temp = arr[mid];
+                arr[mid]=arr[hi];
+                arr[hi] = temp;
+                hi--;}
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+
+        }
+    }
+}
